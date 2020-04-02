2f4bb254101e2305134cd3ddf6072694e7ba68d0
Rileydevlab <59721362+Rileydevlab@users.noreply.github.com>
2020-04-01 20:33:29 -0400

    Final for first appstore release

--- Script/Startmenu.gd
+++ Script/Startmenu.gd
@@ -23,3 +23,7 @@ func _on_StartGamehighscores_pressed():
 func _on_StartGamecredits_pressed():
 	get_tree().change_scene("res://Tscn/Credits.tscn")
 	pass
+
+func _on_StartGamecontrol_pressed():
+	get_tree().change_scene("res://Tscn/Controls.tscn")
+	pass
